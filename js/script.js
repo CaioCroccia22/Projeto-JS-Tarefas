88efc4148fbd7be9823f13d2773bc75ccb85f05d
caiocroccia22 <caiocroccia22@gmail.com>
2023-07-01 18:29:54 -0300

    Seleção de elementos JS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Seleção de elementos
+    const todoForme = document.querySelector("#form-principal");
+    const todoInput = document.querySelector("#input-tarefas");
+    const todoList = document.querySelector("#todo-list");
+    const editForm = document.querySelector("#form-edit");
+    const editInput = document.querySelector("#input-tarefas");   
+    const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+//Funções
+
+//Eventos
