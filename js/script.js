25735d485efcdd5a455fa4a3eacc66b8843de327
caiocroccia22 <caiocroccia22@gmail.com>
2023-07-04 19:51:01 -0300

    Criação função adicionar a lista

--- js/script.js
+++ js/script.js
@@ -9,6 +9,50 @@
 
 //Funções
 
+//Criação do elemento
+const saveTodo = (text) =>{
+
+    //criando um elemento
+    const todo = document.createElement("div")
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+
+    //Inserindo o elemento h3 no elemento div(todo)
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button")
+    //adicionando uma classe ao elemento criado
+    doneBtn.classList.add("finished-todo");
+    //Inserindo o icone dentro do botão
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button")
+    //adicionando uma classe ao elemento criado
+    editBtn.classList.add("edit-todo");
+    //Inserindo o icone dentro do botão
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button")
+    //adicionando uma classe ao elemento criado
+    deleteBtn.classList.add("remove-todo");
+    //Inserindo o icone dentro do botão
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn);
+
+
+    //Colocando a constante todo na lista geral
+    todoList.appendChild(todo);
+
+
+    //Limpar o texto
+    todoInput.value = "";
+    todoInput.focus();
+
+}
 
 //Eventos
 todoForme.addEventListener("submit", (e) => {
@@ -17,6 +61,6 @@ todoForme.addEventListener("submit", (e) => {
     const inputValue = todoInput.value;
 
     if (inputValue) {
-        console.log(inputValue);
+        saveTodo(inputValue)
     }
 });
