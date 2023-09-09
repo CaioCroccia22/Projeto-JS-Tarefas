2c52cce948a76b638450188dcefcaa5c0a843f19
caiocroccia22 <caiocroccia22@gmail.com>
2023-09-08 22:12:46 -0300

    Finalizando o saveTodo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Seleção de elementos
-    const todoForm = document.querySelector("#todo-form");
-    const todoInput = document.querySelector("#todo-input");
-    const todoList = document.querySelector("#todo-list");
-    const editForm = document.querySelector("#edit-form");
-    const editInput = document.querySelector("#edit-input");   
-    const cancelEditBtn = document.querySelector("#cancel-edit-btn");
-    const searchInput = document.querySelector("#erase-button");
-    const filterBtn = document.querySelector("#filter-select");
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
 
 //Funções
 //Função que vai receber um text como parametro e vai retornar
@@ -20,7 +20,27 @@ const saveTodo = (text) => {
     const todoTitle = document.createElement("h3")
     //Vai ser o texto que recebe pela função, ou seja o texto digita no input
     todoTitle.innerHTML = text
-    todo.appendChild(todoTitle)
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo);
+
+
+
 }
 
 
@@ -30,10 +50,10 @@ todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let Inputvalue = todoInput.value;
 
-    if(Inputvalue != 0){
+    if (Inputvalue != 0) {
         saveTodo(Inputvalue);
     }
-    
+
     console.log(Inputvalue);
 })
 
