38970d926eeb442d01a1d426db7d064c891cc892
caiocroccia22 <caiocroccia22@gmail.com>
2023-09-08 22:05:56 -0300

    Inicio da função saveTodo

--- js/script.js
+++ js/script.js
@@ -8,15 +8,32 @@
     const searchInput = document.querySelector("#erase-button");
     const filterBtn = document.querySelector("#filter-select");
 
-
-    
-
 //Funções
+//Função que vai receber um text como parametro e vai retornar
+//A div de class todo
+
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3")
+    //Vai ser o texto que recebe pela função, ou seja o texto digita no input
+    todoTitle.innerHTML = text
+    todo.appendChild(todoTitle)
+}
 
 
+//Eventos
 todoForm.addEventListener("submit", (e) => {
 
     e.preventDefault();
-    console.log("Foii")
+    let Inputvalue = todoInput.value;
+
+    if(Inputvalue != 0){
+        saveTodo(Inputvalue);
+    }
+    
+    console.log(Inputvalue);
 })
 
